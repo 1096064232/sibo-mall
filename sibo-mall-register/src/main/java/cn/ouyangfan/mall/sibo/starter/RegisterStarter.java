9f51de2e13d2bf9f41acd46171a6994735434434
ouyang <1096064232@qq.com>
2020-11-23 13:59:09 +0800

    增加eureka注册中心

--- sibo-mall-register/src/main/java/cn/ouyangfan/mall/sibo/starter/RegisterStarter.java
+++ sibo-mall-register/src/main/java/cn/ouyangfan/mall/sibo/starter/RegisterStarter.java
@@ -1,4 +1,14 @@
 package cn.ouyangfan.mall.sibo.starter;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication(scanBasePackages = {"cn.ouyangfan.mall.sibo"})
 public class RegisterStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterStarter.class,args);
+    }
 }
