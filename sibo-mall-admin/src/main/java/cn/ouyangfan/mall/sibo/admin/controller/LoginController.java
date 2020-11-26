44b53dfa2933718cd32a9df388417ded0ec34e2f
ouyang <1096064232@qq.com>
2020-11-26 17:34:47 +0800

    调试sso单点登录

--- sibo-mall-admin/src/main/java/cn/ouyangfan/mall/sibo/admin/controller/LoginController.java
+++ sibo-mall-admin/src/main/java/cn/ouyangfan/mall/sibo/admin/controller/LoginController.java
@@ -26,13 +26,13 @@ public class LoginController {
     private String oauthServiceUrl;
 
     /**
-     *  获取token的地址
+     * 获取token的地址
      */
     @Value("${sso.authorize-url}")
     private String authorizeUrl;
 
     /**
-     *  admin的回调函数地址
+     * admin的回调函数地址
      */
     @Value("${sso.callback-url}")
     private String callbackUrl;
@@ -40,8 +40,14 @@ public class LoginController {
     @Value("${sso.client-id}")
     private String clientId;
 
+    @Value("${sso.client-secret}")
+    private String clientSecret;
+
     @Value("${sso.scope}")
     private String scope;
+
+    @Value("${sso.front-page}")
+    private String frontPage;
 
 
     private RestTemplate restTemplate = new RestTemplate();
@@ -49,10 +55,10 @@ public class LoginController {
 
     @GetMapping("/admin/login")
     public void adminSSO(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        StringBuffer ssoUrl = new StringBuffer(authorizeUrl+"?response_type=code");
-        ssoUrl.append("&client_id="+clientId)
-                .append("&scope="+scope)
-                .append("&redirect_uri="+callbackUrl);
+        StringBuffer ssoUrl = new StringBuffer(authorizeUrl + "?response_type=code");
+        ssoUrl.append("&client_id=" + clientId)
+                .append("&scope=" + scope)
+                .append("&redirect_uri=" + callbackUrl);
         response.sendRedirect(ssoUrl.toString());
     }
 
@@ -61,16 +67,20 @@ public class LoginController {
         log.info("根据授权码获取token");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setBasicAuth("04e6337ae6c7435d8b2eda995878fe0e", "04e6337ae6c7435d8b2eda995878fe0e");
+        headers.setBasicAuth(clientId, clientSecret);
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("code", code);
         params.add("grant_type", "authorization_code");
         params.add("redirect_uri", callbackUrl);
         HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
 
-        ResponseEntity<TokenInfo> token = restTemplate.exchange(oauthServiceUrl, HttpMethod.POST, entity, TokenInfo.class);
-        log.info("token is:" + token.getBody());
-        System.err.println(token.getBody().getAccess_token());
+        ResponseEntity<TokenInfo> tokenResponse = restTemplate.exchange(oauthServiceUrl, HttpMethod.POST, entity, TokenInfo.class);
+        log.info("获取token接口返回的信息是:{}", tokenResponse.toString());
+        if (tokenResponse.getStatusCode() != HttpStatus.OK) {
+            response.sendRedirect(frontPage + "?code=500&errMessage=获取token信息失败！");
+        }
+        response.sendRedirect(frontPage + "?code=200&token=" + tokenResponse.getBody().getAccess_token()
+                + "&refreshToken=" + tokenResponse.getBody().getRefresh_token());
     }
 
     @GetMapping("/me")
